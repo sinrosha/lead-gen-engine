b2a5f2c8fc5899cfe5d8e8a9a4e45cd0e636c920
Adhiraj Singh <adhiraj.singh@giift.com>
2022-05-14 20:31:01 +0530

    added faq component

--- components/FAQ/index.jsx
+++ components/FAQ/index.jsx
@@ -2,28 +2,24 @@ import styles from  "./styles.module.css"
 
 const FAQ = () =>{
     return (
-      <section id="FAQ" className={styles.faq}>
+      <section id="FAQ" className={styles.faq} >
             <div className="md:container mx-auto px-4">
                 <div className="grid grid-cols-1">
                     <div className="heading">
-                    <h2 className={styles.smallTitle}>FAQ</h2>
-                    <h1 className={styles.largeTitle}>Frequently Asked <br/> Questions</h1>
+                    <h3 className={styles.subTitle}>FAQ</h3>
+                    <h2 className={styles.title}>Frequently Asked <br/> Questions</h2>
                     </div>
                 </div>
-                <div className={` ${"dfs"} grid grid-cols-1`}>
-                    <div className="">
+                <div className={` ${styles.content} grid grid-cols-1 `}>
+                    <div className="py-4">
                         <div className={styles.question}>what</div>
                         <div className={styles.answer}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis tempora sunt amet qui aperiam quasi minus voluptatibus molestias, eius rerum earum ducimus ullam laborum quisquam iusto pariatur reprehenderit expedita cum!</div>
                     </div>
-                    <div className="">
+                    <div className="pb-4">
                         <div className={styles.question}>what</div>
                         <div className={styles.answer}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis tempora sunt amet qui aperiam quasi minus voluptatibus molestias, eius rerum earum ducimus ullam laborum quisquam iusto pariatur reprehenderit expedita cum!</div>
                     </div>
-                    <div className="">
-                        <div className={styles.question}>what</div>
-                        <div className={styles.answer}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis tempora sunt amet qui aperiam quasi minus voluptatibus molestias, eius rerum earum ducimus ullam laborum quisquam iusto pariatur reprehenderit expedita cum!</div>
-                    </div>
-                    <div className="">
+                    <div className="pb-4">
                         <div className={styles.question}>what</div>
                         <div className={styles.answer}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis tempora sunt amet qui aperiam quasi minus voluptatibus molestias, eius rerum earum ducimus ullam laborum quisquam iusto pariatur reprehenderit expedita cum!</div>
                     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout"
 import HomeHero from "../components/HomeHero"
-import Testimonial from "../components/Testimonail"
 import content from "../content/home.json"
 import FAQ from "../components/FAQ"
 
@@ -8,7 +7,7 @@ const Home = () => {
   return (
     <Layout content={content}>
       <HomeHero />
-      <Testimonial />
+      <FAQ />
     </Layout>
   )
 }
