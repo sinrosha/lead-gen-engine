77e530c3f6f44d4729a7b7bbbaa0002232033d61
Adhiraj Singh <adhiraj.singh@giift.com>
2022-05-15 18:25:32 +0530

    polished UI for the FAQ component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,13 @@ import Layout from "../components/Layout"
 import HomeHero from "../components/HomeHero"
 import content from "../content/home.json"
 import FAQ from "../components/FAQ"
+import Testimonial from "../components/Testimonail"
 
 const Home = () => {
   return (
     <Layout content={content}>
       <HomeHero />
+      <Testimonial/>
       <FAQ />
     </Layout>
   )
