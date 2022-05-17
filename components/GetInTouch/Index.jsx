8239ca75ff308c1a37a6c82ed56011cfd54ee397
Roshan Singh <roshan.singh@gupshup.io>
2022-05-17 14:00:27 +0530

    polished CTA Component

--- components/GetInTouch/Index.jsx
+++ components/GetInTouch/Index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import styles from "./style.module.css"
 function GetInTouch() {
   return (
-    <div>
-        <div className={`getInTouch-section relative py-12 md:py-16 bg-light-green ${styles.getInTouchSection}`}>
-            <div className="container">
-                 <div className="mr-auto ml-auto text-center">
-                     <h4 className='text-white block w-full mt-10 text-center text-lg font-bold font-sen mb-2.5'>Get in touch</h4>
-                     <h3 className='text-white text-7xl font-semibold text-center font-poppins mt-5 mb-3'>Ready to hire?</h3>
-                     <button className={`bg-black-bg inline-block py-3 px-6 font-sen text-white font-bold text-sm rounded mt-8 ${styles.btnBlk}`}>BOOK DEMO</button>
-                 </div>
-            </div>
+    <section id="CTA">
+      <div className={`relative py-12 md:py-16 bg-light-green ${styles.getInTouchSection}`} 
+        style={{"background-image": "linear-gradient(27deg, #3ccf91 32%, #68d4ff)"}}>
+        <div className="mx-auto text-center">
+          <h4 className='text-white block w-full text-center text-lg font-bold font-sen mb-2.5'>
+            Get in touch
+          </h4>
+          <h3 className='text-white text-7xl font-semibold text-center font-poppins mt-5 mb-3'>
+            Ready to hire?
+          </h3>
+          <button className="bg-black-bg inline-block py-3 px-6 font-sen text-white font-bold text-sm rounded mt-8 hover:opacity-80 transition">BOOK DEMO</button>
         </div>
-    </div>
+      </div>
+    </section>
   )
 }
 
