489bca0caa6ea0b7a691d732842bcdb0bbbbd026
Roshan Singh <roshan.singh@gupshup.io>
2022-05-18 17:37:11 +0530

    bug fix

--- components/BrandSlider/index.jsx
+++ components/BrandSlider/index.jsx
@@ -7,7 +7,7 @@ const BrandSlider = ({brands}) => {
       <h3 className="font-bold text-center text-white text-3xl md:text-5xl font-poppins">Some of the Brands we work on</h3>
       <div className="mx-auto flex flex-wrap justify-center md:justify-between px-8 w-4/5 pt-8 gap-5">
         {brands.map( brand => (
-          <div className="logo w-32">
+          <div className="logo w-32" key={brand.id}>
             <Image width="300px" height="150px" src={brand.src} alt={brand.name} />
           </div>
         ))}
