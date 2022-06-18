f2e1845f22aa49b8b400ba751054009c604a6b41
Roshan Singh <roshan.singh@gupshup.io>
2022-06-18 15:26:16 +0530

    bug gix

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -8,23 +8,27 @@ const Layout = ({content, children}) => {
   const [cookies, setCookie, removeCookie] = useCookies(["visitor_info"]);
 
   useEffect(() => {
-    if(!cookies.vistor_info){
+    if(!cookies.visitor_info){
       fetch("https://ipinfo.io/json?token=36b069382aeb35")
       .then( res => res.json())
       .then( res => {
+        const {city, country, hostname, ip, org, postal, region, timezone, loc} = res;
         setCookie("visitor_info", JSON.stringify(res),{path:"/"});
         window.dataLayer.push({
-          event: "new_user",
-          value: res
+          event: "newUser",
+          data: {
+            city,
+            country,
+            hostname,
+            ip,
+            loc,
+            org,
+            postal,
+            region,
+            timezone
+          }
         })
       })
-    } else {
-      let data={};
-      try {
-        data = JSON.parse(res) 
-      }catch(err){
-        console.error(err);
-      }
     }
   }, []);
 
