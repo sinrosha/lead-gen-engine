5706fb7141f5bc8558882a749f3901f85a50b45c
Adhiraj Singh <adhiraj.singh@giift.com>
2022-06-19 03:19:25 +0530

    icon

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,15 +1,15 @@
 import { SiInstagram } from 'react-icons/Si';
 import { GrFacebook } from 'react-icons/Gr';
-import { SiTwitter } from 'react-icons/Si';
+import { SiGmail } from 'react-icons/Si';
 
 const Footer = () => {
   return (
     <header className="sticky top-0 z-10 py-6 bg-black-bg">
-      <div className="flex md:flex-row flex-col justify-between mx-auto w-4/5 items-center">
+      <div className="flex flex-col justify-between mx-auto w-4/5 items-center md:flex-row-reverse">
         <div className="flex flex-row">
-      <a href=""><SiInstagram/></a>
-      <a href="" className="px-6 pb-3 md:pb-0"><GrFacebook/></a>
-      <a href=""><SiTwitter/></a>
+      <a href="https://instagram.com/handymanservicespvt?igshid=YmMyMTA2M2Y="><SiInstagram className='text-white' size={20}/></a>
+      <a href="https://www.facebook.com/HandyMan-Services-115098371200634/" className="px-6 pb-3 md:pb-0"><GrFacebook className='text-white' size={20}/></a>
+      <a href="mailto:handymanservicespvt@gmail.com"><SiGmail className='text-white' size={22}/></a>
       </div>
       <div>
         <p className="text-white">Copyright © 2022 Handyman</p>
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="sticky top-0 z-10 py-4 md:py-5 bg-black-bg">
       <div className="flex justify-between mx-auto w-10/12 md:w-4/5 items-center">
         <div className="font-sen flex text-white text-3xl">
-          <Image src="/android-icon-36x36.png" width="32" height="32" alt="Logo" /> <span className="pl-2 md:pl-4 text-2xl md:text-3xl">HandyMan</span>
+          <Image src="/android-icon-36x36.png" width="36" height="36" alt="Logo" /> <span className="pl-2 md:pl-4 text-2xl md:text-3xl">HandyMan</span>
         </div>
         <div className="cta px-4 py-1 md:px-6 md:py-2 rounded-md bg-pink">
           <a href="tel:08459982237" className="font-medium md:font-semibold md:font-base text-white font-sen">
