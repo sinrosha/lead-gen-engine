ad132e27e5d13e6229c5165dc10993d3227fb328
Roshan Singh <roshan.singh@gupshup.io>
2022-06-18 15:17:05 +0530

    adding ipinfo tracking

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,6 +1,33 @@
+import React, {useState, useEffect} from "react"
 import Meta from "../Meta"
 import Header from '../Header'
+import { useCookies } from "react-cookie";
+
 const Layout = ({content, children}) => {
+
+  const [cookies, setCookie, removeCookie] = useCookies(["visitor_info"]);
+
+  useEffect(() => {
+    if(!cookies.vistor_info){
+      fetch("https://ipinfo.io/json?token=36b069382aeb35")
+      .then( res => res.json())
+      .then( res => {
+        setCookie("visitor_info", JSON.stringify(res),{path:"/"});
+        window.dataLayer.push({
+          event: "new_user",
+          value: res
+        })
+      })
+    } else {
+      let data={};
+      try {
+        data = JSON.parse(res) 
+      }catch(err){
+        console.error(err);
+      }
+    }
+  }, []);
+
   return (
     <>
       <Meta content={content} />
