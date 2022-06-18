d21d36f8496baae4dcb5dbf1b1887e6f0d90161f
Adhiraj Singh <adhiraj.singh@giift.com>
2022-06-19 03:00:09 +0530

    footer icon added

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,13 +1,15 @@
-
+import { SiInstagram } from 'react-icons/Si';
+import { GrFacebook } from 'react-icons/Gr';
+import { SiTwitter } from 'react-icons/Si';
 
 const Footer = () => {
   return (
     <header className="sticky top-0 z-10 py-6 bg-black-bg">
       <div className="flex md:flex-row flex-col justify-between mx-auto w-4/5 items-center">
         <div className="flex flex-row">
-      <a href=""><img src="/logos/facebook.png" className="w-8"/></a>
-      <a href="" className="px-6 pb-3 md:pb-0"><img src="/logos/instagram.png" className="w-8"/></a>
-      <a href=""><img src="/logos/twitter.png" className="w-8"/></a>
+      <a href=""><SiInstagram/></a>
+      <a href="" className="px-6 pb-3 md:pb-0"><GrFacebook/></a>
+      <a href=""><SiTwitter/></a>
       </div>
       <div>
         <p className="text-white">Copyright © 2022 Handyman</p>
