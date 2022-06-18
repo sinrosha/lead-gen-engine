5bd4b3cbaefb713961b04d46f0fc0b543419eb87
Roshan Singh <roshan.singh@gupshup.io>
2022-06-19 01:14:57 +0530

    added visitor logging functionality

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -7,28 +7,69 @@ const Layout = ({content, children}) => {
 
   const [cookies, setCookie, removeCookie] = useCookies(["visitor_info"]);
 
+  const sentUserDataToSheet = (res) => {
+
+    const {city, country, hostname, ip, org, postal, region, timezone, loc} = res;
+
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbwzOjgqhveT0OqGbWGB7SacN1AG3Jdwqt8gzqz4B_-iKkvjDr553H-w3ydFUfhfk1haIg/exec";
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("Date", new Date());
+    urlencoded.append("City", city);
+    urlencoded.append("Country", country);
+    urlencoded.append("Hostname", hostname);
+    urlencoded.append("IP", ip);
+    urlencoded.append("Location", loc);
+    urlencoded.append("Org", org);
+    urlencoded.append("Postal", postal);
+    urlencoded.append("Region", region);
+    urlencoded.append("Timezone", timezone);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch(scriptUrl, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
+
+  const sendEventToGTM = (res) => {
+    const {city, country, hostname, ip, org, postal, region, timezone, loc} = res;
+    window.dataLayer.push({
+      event: "newUser",
+      data: {
+        city,
+        country,
+        hostname,
+        ip,
+        loc,
+        org,
+        postal,
+        region,
+        timezone
+      }
+    })
+  }
+
   useEffect(() => {
     if(!cookies.visitor_info){
       fetch("https://ipinfo.io/json?token=36b069382aeb35")
       .then( res => res.json())
       .then( res => {
-        const {city, country, hostname, ip, org, postal, region, timezone, loc} = res;
         setCookie("visitor_info", JSON.stringify(res),{path:"/"});
-        window.dataLayer.push({
-          event: "newUser",
-          data: {
-            city,
-            country,
-            hostname,
-            ip,
-            loc,
-            org,
-            postal,
-            region,
-            timezone
-          }
-        })
+        sendEventToGTM(res);
+        sentUserDataToSheet(res);
       })
+    } else {
+      sentUserDataToSheet(cookies.visitor_info);
     }
   }, []);
 
